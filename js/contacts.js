c1200609e5448227290a355176cef2911d59560b
Артем Кан <artiomkan@yandex.ru>
2021-05-29 10:18:10 +0400

    init

--- js/contacts.js
+++ js/contacts.js
@@ -1,11 +1,21 @@
 'use strict'
+class boombasterFaster{
+    constructor(person, typical, boombaster) {
+        person.addEventListener('mouseover', ()=>{
+            person.src = boombaster
+        })
+        person.addEventListener('mouseleave', ()=>{
+            person.src = typical
+        })
+    }
+}
+
 let max = document.querySelector('#max'),
     maxTypical = 'img/attachments/max.png',
-    maxBoombaster = 'img/attachments/max-boombaster.png'
+    maxBoombaster = 'img/attachments/max-boombaster.png',
+    egor = document.querySelector('#egor'),
+    egorTypical = 'img/attachments/egor.jpg',
+    egorBoombaster = 'img/attachments/egor-boombaster.jpg'
 
-max.addEventListener('mouseover', ()=>{
-    max.src = maxBoombaster
-})
-max.addEventListener('mouseleave', ()=>{
-    max.src = maxTypical
-})
+let bf1 = new boombasterFaster(max, maxTypical, maxBoombaster),
+    bf2 = new boombasterFaster(egor, egorTypical, egorBoombaster)
